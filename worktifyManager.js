e78917295f56c96d2d57ecbfa27f730e947d68dd
Alan Hryniewicz <alan.07-sl@hotmail.com>
2018-01-19 17:32:04 -0300

    Create worktify manager
    First baseline of the main FaaS that's currently running on webtask.io.
    This code covers the OAuth flow of Spotify until the 5th step.

---
+++ worktifyManager.js
@@ -0,0 +1,124 @@
+var util = require('util');
+var querystring = require('querystring');
+var http = require('http');
+var https = require('https');
+var fs = require('fs');
+
+/**
+* @param context {WebtaskContext}
+*/
+module.exports = function(context, cb) {
+  const authorizeUrl = 'https://accounts.spotify.com/authorize?client_id=%s&response_type=code&redirect_uri=%s&scope=user-modify-playback-state';
+  const clientId = '3b8e56b6553f442aa483fe488eca5b30';
+  const clientSecret = '2d9c70863026407db705645ad2f2aaf5';
+  const webTaskUrl = 'https://wt-1421b0d761ddd832608482e64eb8e4fc-0.run.webtask.io/worktify-main';
+  const spotifyAccountServiceHost = 'accounts.spotify.com'
+  const spotifyAccountServicePath = '/api/token'
+
+  if(typeof context.body !== "undefined") {
+
+    //if it comes from slack then we will have a text field in the body.
+    if(typeof context.body.text !== "undefined") {
+      var args = context.body.text;
+      var argsArray = args.split(" ");
+      var command = argsArray[0];
+      var arrayLen = argsArray.length;
+
+      switch (command) {
+        case 'login':
+          login(arrayLen);
+          break;
+        case 'logout':
+          logout(arrayLen);
+          break;
+        case 'volume':
+          volume(argsArray);
+          break;
+        case 'oncall on':
+          cb(null, 'Seteaste oncall on');
+          break;
+          case 'oncall off':
+          cb(null, 'Seteaste oncall off');
+          break;
+        case 'help':
+          cb(null, 'Texto de ayuda');
+          break;
+        default:
+          cb(null,'Para más información sobre las funcionalidades de worktify por favor usa /worktify help');
+        break;
+      }
+    } else {
+        cb(null, 'Para más información sobre las funcionalidades de worktify por favor usa /worktify help'); 
+    }
+  } else { //if we don't have that then we need to check if it's the callback of spotify.
+      if(typeof context.query.code !== "undefined") {
+        PostCode(context.query.code);
+      }
+  }
+    
+
+  /* Functions to make requests. */
+
+  function PostCode(codestring) {
+    // Build the post string from an object
+    var post_data = querystring.stringify({
+        'grant_type' : 'authorization_code',
+        'code' : codestring,
+        'redirect_uri' : webTaskUrl
+    });
+
+    // An object of options to indicate where to post to
+    var post_options = {
+        host: spotifyAccountServiceHost,
+        path: spotifyAccountServicePath,
+        method: 'POST',
+        headers: {
+            'Content-Type': 'application/x-www-form-urlencoded',
+            'Authorization': 'Basic ' + Buffer.from(clientId + ':' + clientSecret).toString('base64')
+        }
+    };
+
+    // Set up the request
+    var post_req = https.request(post_options, function(res) {
+        res.setEncoding('utf8');
+        res.on('data', function (chunk) {
+            console.log('Response: ' + chunk);
+        });
+    });
+
+    // post the data
+    post_req.write(post_data);
+    post_req.end();
+  }    
+  
+  
+  /* Functions to handle each command. */
+  
+  function login(len) {
+    if(len == 1) {
+      cb(null, 'Please login and authorize worktify here:' + util.format(authorizeUrl, clientId, webTaskUrl));
+    } else {
+      cb(null, 'Login command must have no parameters.');
+    }
+  }
+  
+  function logout(len) {
+    if(len == 1) {
+      cb(null, 'You have logged out, thanks for using worktify.'); 
+    } else {
+      cb(null, 'Logout command must have no parameters.');
+    }
+  }
+  
+  function volume(argsArray) {
+    var percentage = argsArray[1];
+    if(argsArray.length == 2 && percentage >= 0 && percentage<= 100) {
+      cb(null, util.format('You set the volume to %d%', percentage));
+    } else {
+      cb(null, 'Volume command just recives an argument with range is 0-100');
+    }
+  }
+  
+};
+
+

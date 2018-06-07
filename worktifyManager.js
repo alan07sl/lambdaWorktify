279884031ffcacc271aa6850c31adf5779e0f750
matias Devoto <matias.devoto@valtech.com>
2018-06-07 15:46:58 -0300

    Prueba obtencion parametros

--- worktifyManager.js
+++ worktifyManager.js
@@ -32,7 +32,7 @@ module.exports = function(context, cb) {
   const redisAccessToken = 'access_token';
 
   const buildings = "palermo1,palermo2,ramos1,ramos2".split(",")
-  const params = context.headers
+  const params = context.headers.user
   
   var token_type;
   var scope;
@@ -154,7 +154,7 @@ module.exports = function(context, cb) {
   
   function login(len,test) {
     if(len == 1) {
-      cb(null, 'Please login and authorize worktify here:'+ params+' BBBB ' + util.format(authorizeUrl, clientId, webTaskUrl));
+      cb(null, 'Please login and authorize worktify here:'+ params+' cccc ' + util.format(authorizeUrl, clientId, webTaskUrl));
     } else {
       cb(null, 'Login command must have no parameters.');
     }

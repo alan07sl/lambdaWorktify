45e878f580919147705f27ae0d95e17c42b370fa
matias Devoto <matias.devoto@valtech.com>
2018-06-07 15:52:34 -0300

    Prueba obtencion parametros

--- worktifyManager.js
+++ worktifyManager.js
@@ -32,7 +32,7 @@ module.exports = function(context, cb) {
   const redisAccessToken = 'access_token';
 
   const buildings = "palermo1,palermo2,ramos1,ramos2".split(",")
-  const params = context.headers[0]
+  const params = context.headers
   
   var token_type;
   var scope;

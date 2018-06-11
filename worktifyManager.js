5adc4ad9971e11732823af34ed2380be01c03d2e
matias Devoto <matias.devoto@valtech.com>
2018-06-11 14:59:25 -0300

    feature HELP

--- worktifyManager.js
+++ worktifyManager.js
@@ -77,12 +77,12 @@ module.exports = function(context, cb) {
                     whatson(arrayLen,user);
                     break;
                 case 'help':
-                    cb(null,{ hello:  `Commands:"
-"/worktify login_reproducer <building>"
-"/worktify login_listener <building>
+                    cb(null,Commands:
+/worktify login_reproducer <building>
+/worktify login_listener <building>
 		/worktify logout
 		/worktify volume <0-100>
-		/worktify whatson`});
+		/worktify whatson);
                     break;
                 default:
                     cb(null,'For more usage information please use: /worktify help');
